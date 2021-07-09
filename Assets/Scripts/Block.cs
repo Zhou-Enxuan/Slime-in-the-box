b203cfa4ce83ba681a7daff872049029945f75cb
Zhou-Enxuan <enzhou64@gmail.com>
2021-07-09 16:00:32 -0700

    finished initallized board

--- Assets/Scripts/Block.cs
+++ Assets/Scripts/Block.cs
@@ -1,8 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Block : MonoBehaviour
 {
-    public Monster monster;
+    public Text textCom;
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        
+    }
 }

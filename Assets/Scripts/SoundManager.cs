bb85b4b7f3ba59f87640639f7c86909f57d59869
MarkMaTeng <markmateng@gmail.com>
2021-07-10 19:41:03 +0800

    Add SoundManager

---
+++ Assets/Scripts/SoundManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+
+    public AudioSource theme;
+    private void Awake()
+    {
+        DontDestroyOnLoad(this);
+
+        theme = this.gameObject.AddComponent<AudioSource>();
+        theme.loop = true;
+
+    }
+
+    private void Start()
+    {
+        theme.Play();
+    }
+
+}

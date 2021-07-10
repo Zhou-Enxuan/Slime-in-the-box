a1d66e94d5d292408fdc49feb685d9c6099e7e39
MarkMaTeng <markmateng@gmail.com>
2021-07-10 22:37:21 +0800

    Add CalculateRandomSize()

--- Assets/Scripts/Monster.cs
+++ Assets/Scripts/Monster.cs
@@ -8,6 +8,8 @@ public class Monster
 
     private int size;
 
+    private Player player;
+
     public Monster(int _size)
     {
         size = _size;
@@ -22,4 +24,81 @@ public class Monster
     {
         return size;
     }
+
+    public void CalculateMonsterSize()
+    {
+        //当玩家是1的时候，八个格子70 % 概率出现1
+
+        //当玩家是2的时候，八个格子70 % 概率出现1，2，3
+
+        //当玩家是3的时候，八个格子90 % 概率出现1，2，3，4，5
+
+        //当玩家是4的时候，八个格子90 % 概率出现2，3，4，5，6
+
+        //当玩家是5的时候，八个格子80 % 概率出现4，5，6
+
+        //当玩家是6的时候，八个格子70 % 概率出现6
+
+        switch (player.size)
+        {
+            case 1:
+                if (Random.value <= 0.7f)
+                    size = 1;
+                else
+                    size = Random.Range(2, 6);
+                break;
+            case 2:
+                if (Random.value <= 0.23f)
+                    size = 1;
+                else if (Random.value <= 0.46f)
+                    size = 2;
+                else if (Random.value <= 0.7f)
+                    size = 3;
+                else
+                    size = Random.Range(4, 6);
+                break;
+            case 3:
+                if (Random.value <= 0.18f)
+                    size = 1;
+                else if (Random.value <= 0.36f)
+                    size = 2;
+                else if (Random.value <= 0.54f)
+                    size = 3;
+                else if (Random.value <= 0.72f)
+                    size = 4;
+                else if (Random.value <= 0.9f)
+                    size = 5;
+                else
+                    size = 6;
+                break;
+            case 4:
+                if (Random.value <= 0.18f)
+                    size = 6;
+                else if (Random.value <= 0.36f)
+                    size = 2;
+                else if (Random.value <= 0.54f)
+                    size = 3;
+                else if (Random.value <= 0.72f)
+                    size = 4;
+                else if (Random.value <= 0.9f)
+                    size = 5;
+                else
+                    size = 1;
+                break;
+            case 5:
+                if (Random.value <= 0.23f)
+                    size = 4;
+                else if (Random.value <= 0.46f)
+                    size = 5;
+                else if (Random.value <= 0.7f)
+                    size = 6;
+                break;
+            case 6:
+                if (Random.value <= 0.7f)
+                    size = Random.Range(1, 3);
+                break;
+        }
+    }
+
+    
 }

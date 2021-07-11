e4a90c871af65bbbc28dc4956e6761482a0adb22
MarkMaTeng <markmateng@gmail.com>
2021-07-11 16:29:10 +0800

    Add Restart Game

---
+++ Assets/Scripts/GameOverWindows.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameOverWindows : MonoBehaviour
+{
+    Text score;
+    Button restart;
+    public Blocks blocks;
+    public Player player;
+
+    private void Awake()
+    {
+        player = FindObjectOfType<Player>().GetComponent<Player>();
+        //blocks = GetComponent<Blocks>();
+        //player = GetComponent<Player>();
+        score = transform.GetChild(1).GetComponent<Text>();
+        restart = transform.GetChild(2).GetComponent<Button>();
+
+        restart.onClick.AddListener(RestartGame);
+    }
+
+    private void RestartGame()
+    {
+        GameManager.instance.score = 0;
+        //blocks.UnlockControl();
+        player.ResetSize();
+        GameManager.instance.GameOver = false;
+        GameManager.instance.counter = false;
+        Destroy(transform.gameObject.GetComponentInParent<Canvas>().gameObject);
+    }
+
+}
